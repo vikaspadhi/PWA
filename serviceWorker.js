245cbe3a3e0ff6739905acd788b51e88f11da18c
vikaspadhi <vikaspadhi5@gmail.com>
2022-01-05 21:06:27 +0530

    Initial commit

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticDevCoffee = "newsapp-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "style.css",
+  "/js/main.js",
+  "/coffee1.jpg",
+];
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
